ae76d9e8d9f0d66b9de5fcd8ded5f10e00225ace
Srilatha Karancheti <slkarancheti@gmail.com>
2024-10-05 07:39:21 -0400

    Initial Commit

---
+++ SwiftUINavigation/SwiftUINavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigationApp.swift
+//  SwiftUINavigation
+//
+//  Created by Srilatha on 2024-10-05.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
